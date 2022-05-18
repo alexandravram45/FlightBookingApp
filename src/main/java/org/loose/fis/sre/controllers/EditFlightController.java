98d7581ffd24905cf1c4f9c6072d92e5950bcc07
Alexia Balamoti <alexiabalamoti@gmail.com>
2022-05-18 20:34:42 +0300

    edit flight updated and admin choices added

--- src/main/java/org/loose/fis/sre/controllers/EditFlightController.java
+++ src/main/java/org/loose/fis/sre/controllers/EditFlightController.java
@@ -1,4 +1,28 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class EditFlightController {
+
+    @FXML
+
+    private TextField cityA, cityB, oldTime, newTime;
+
+    @FXML
+
+    private DatePicker oldDate, newDate;
+
+    @FXML
+
+    private Button editFlight;
+
+     @FXML
+
+    public void editFlightButton () {
+
+    }
+
 }
